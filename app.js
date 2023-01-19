c77b8a67bb1425ed16431ec126d47143de9a7e4b
Jared Russell <jaredhrussell2016@gmail.com>
2023-01-19 10:41:03 -0500

    Add clear button and title (clear button doesnt work properly)

--- app.js
+++ app.js
@@ -1,19 +1,49 @@
 const box = document.querySelector('.box');
 
 
-let count = 256;
+let count = Math.sqrt(256);
+count = count * count;
 while (count != 0){
     let square = document.createElement('div');
     square.classList.add('square');
+    square.style.width = "20px"
     box.appendChild(square);
     count--;   
 }
 
-//let colors = ['red', 'blue', 'green', 'teal', 'rosybrown', 'tan', 'plum', 'saddlebrown'];
+let colors = ['red', 'blue', 'green', 'teal', 'rosybrown', 'tan', 'plum', 'saddlebrown'];
 let squares = document.querySelectorAll('.square');
 squares.forEach(function(elem){
     elem.addEventListener('mouseover', () =>{
-        elem.style.backgroundColor = 'green'
+        elem.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
 })
-    //= colors[Math.floor(Math.random() * colors.length)];
 })
+
+
+
+
+
+//clear button
+let clearBtn = document.querySelector('.restart');
+clearBtn.addEventListener('click', () =>{
+    byeSquare = document.querySelectorAll('.square');
+    byeSquare.forEach(element => {
+        box.removeChild(element);
+    })
+})
+
+clearBtn.addEventListener('click', () => {
+    let newCount = prompt("Enter number of squares: ");
+    newCount = Number(newCount);
+    squares.forEach(element => {
+        element.style.backgroundColor = 'white';
+        while (newCount != 0){
+            let squares = document.createElement('div');
+            squares.classList.add('square');
+            squares.style.width = "20px"
+            box.appendChild(squares);
+            newCount--;   
+        }
+    })  
+})
+
